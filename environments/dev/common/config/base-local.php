7e294909ca065726e6bdc1e991e66206811976ce
Haluk Demir <web@halukdemir.com>
2015-01-06 00:01:46 +0200

    env

--- environments/dev/common/config/base-local.php
+++ environments/dev/common/config/base-local.php
@@ -6,7 +6,7 @@ return [
         ],
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=yiipro', // localhost is much slower than 127.0.0.1
+            'dsn' => 'mysql:host=localhost;dbname=yiipro', // localhost is much slower than 127.0.0.1
             'username' => 'root',
             'password' => 'mavi1233',
             'charset' => 'utf8'
