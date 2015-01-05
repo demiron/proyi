0c48c74dd575160b114956e4f4859c65cbb6e016
Haluk Demir <web@halukdemir.com>
2015-01-06 01:30:18 +0200

    no message

--- frontend/config/_urlManager.php
+++ frontend/config/_urlManager.php
@@ -2,7 +2,6 @@
 return [
     'class'=>'yii\web\UrlManager',
     'enablePrettyUrl'=>true,
-    'urlFormat'=>'path',
     'showScriptName'=>false,
     'rules'=> [
         ['pattern'=>'page/<alias>', 'route'=>'page/view']
